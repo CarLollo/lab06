868875cb4e5e76178d69ab2221733de9156ac6c3
LorenzoCarletti <lorenzo.carletti3@studio.unibo.it>
2023-10-30 12:54:04 +0100

    Es. 61 e 62

--- 61-git-remotes-merge-conflict/README.md
+++ 61-git-remotes-merge-conflict/README.md
@@ -11,16 +11,63 @@ Prima di eseguire ogni operazione sul worktree o sul repository,
 si verifichi lo stato del repository con `git status`.
 
 1. Si cloni localmente il repository
+git clone https://github.com/CarLollo/OOP-git-merge-conflict-test.git 'Risoluzione merge coflict'
+Cloning into 'Risoluzione merge coflict'...
+remote: Enumerating objects: 12, done.
+remote: Counting objects: 100% (4/4), done.
+remote: Compressing objects: 100% (3/3), done.
+remote: Total 12 (delta 1), reused 1 (delta 1), pack-reused 8
+Receiving objects: 100% (12/12), done.
+Resolving deltas: 100% (2/2), done.
+
 2. Ci si assicuri di avere localmente entrambi i branch remoti
+git checkout feature
+Switched to a new branch 'feature'
+branch 'feature' set up to track 'origin/feature'.
+
+git branch
+* feature
+  master
+
 3. Si faccia il merge di `feature` dentro `master`, ossia: si posizioni la `HEAD` su `master`
    e da qui si esegua il merge di `feature`
+git merge feature
+Auto-merging HelloWorld.java
+CONFLICT (content): Merge conflict in HelloWorld.java
+Automatic merge failed; fix conflicts and then commit the result.
+
 4. Si noti che viene generato un **merge conflict**!
+
 5. Si risolva il merge conflict come segue:
    - Il programma Java risultante deve stampare sia il numero di processori disponibili
      (funzionalità presente su `master`)
      che il nome dell'autore del file
      (funzionalità presente su `feature`)
+
 6. Si crei un nuovo repository nel proprio github personale
+
 7. Si aggiunga il nuovo repository creato come **remote** e si elenchino i remote
+git remote add 'MioRepository' https://github.com/CarLollo/RepositorydiProva.git
+
+git remote
+MioRepository
+origin
+
 8. Si faccia push del branch `master` sul proprio repository
+
+git push -u MioRepository 
+info: please complete authentication in your browser...
+Enumerating objects: 15, done.
+Counting objects: 100% (15/15), done.
+Delta compression using up to 12 threads
+Compressing objects: 100% (11/11), done.
+Writing objects: 100% (15/15), 1.58 KiB | 230.00 KiB/s, done.
+Total 15 (delta 4), reused 10 (delta 2), pack-reused 0
+remote: Resolving deltas: 100% (4/4), done.
+To https://github.com/CarLollo/RepositorydiProva.git
+ * [new branch]      master -> master
+branch 'master' set up to track 'MioRepository/master'.
+
 9. Si setti il branch remoto `master` del nuovo repository come *upstream* per il proprio branch `master` locale
+git branch --set-upstream-to=MioRepository/master
+branch 'master' set up to track 'MioRepository/master'.
--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,13 +1,28 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
  *
  */
 public final class UseListsAndMaps {
+
+    private static final int ELEMS_ADD = 100_000;
+    private static final int ELEMS_READ = 1000;
+    private static final int START_INS = 1000;
+    private static final int END_INS = 2000;
+    private static final long AFRICA_POPULATION = 1_110_635_000L;
+    private static final long AMERICAS_POPULATION = 972_005_000;
+    private static final long ANTARTICA_POPULATION = 0;
+    private static final long ASIA_POPULATION = 4_298_723_000L;
+    private static final long EUROPE_POPULATION = 742_452_000;
+    private static final long OCEANIA_POPULATION = 38_304_000;
 
     private UseListsAndMaps() {
     }
@@ -21,19 +36,30 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+        ArrayList<Integer> a = new ArrayList<>();
+        for (int i = START_INS; i < END_INS; i++) {
+            a.add(i);
+        }
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+        LinkedList<Integer> l = new LinkedList<>(a);
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+        var temp = a.get(a.size()-1);
+        a.set(a.size()-1, a.get(0));
+        a.set(0, temp);
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (int it : a) {
+            System.out.println(it);
+        }
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +67,40 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        long timeA = System.nanoTime();
+        for (int i = 1; i <= ELEMS_ADD; i++) {
+            a.add(0, i);
+        }
+        timeA = System.nanoTime() - timeA;
+        long timeL = System.nanoTime();
+        for (int i = 1; i <= ELEMS_ADD; i++) {
+            l.add(0, i);
+        }
+        timeL = System.nanoTime() - timeL;
+        final var millisA = TimeUnit.NANOSECONDS.toMillis(timeA);
+        final var millisL = TimeUnit.NANOSECONDS.toMillis(timeL);
+        System.out.println("Converting " + a.size() + " ints to String and inserting them in a ArrayList took " + timeA + "ns (" + millisA + "ms)");
+        System.out.println("Converting " + l.size() + " ints to String and inserting them in a LinkedList took " + timeL + "ns (" + millisL + "ms)");
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        long time1A = System.nanoTime();
+        for (int i = 1; i <= ELEMS_READ; i++) {
+            a.get(a.size()/2);
+        }
+        time1A = System.nanoTime() - time1A;
+        long time1L = System.nanoTime();
+        for (int i = 1; i <= ELEMS_READ; i++) {
+            l.get(l.size()/2);
+        }
+        time1L = System.nanoTime() - time1L;
+        final var millis1A = TimeUnit.NANOSECONDS.toMillis(time1A);
+        final var millis1L = TimeUnit.NANOSECONDS.toMillis(time1L);
+        System.out.println("Reading " + a.size() + " ints to String and inserting them in a ArrayList took " + time1A + "ns (" + millis1A + "ms)");
+        System.out.println("Reading " + l.size() + " ints to String and inserting them in a LinkedList took " + time1L + "ns (" + millis1L + "ms)");
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -63,8 +117,20 @@ public final class UseListsAndMaps {
          *
          * Oceania -> 38,304,000
          */
+        Map<String, Long> m = new TreeMap<>();
+        m.put("Africa", AFRICA_POPULATION);
+        m.put("Americas", AMERICAS_POPULATION);
+        m.put("Antartica", ANTARTICA_POPULATION);
+        m.put("Asia", ASIA_POPULATION);
+        m.put("Europe", EUROPE_POPULATION);
+        m.put("Oceania", OCEANIA_POPULATION);
         /*
          * 8) Compute the population of the world
          */
+        long tot_pop = 0;
+        for (final long c : m.values()) {
+            tot_pop += c;
+        }
+        System.out.println("Total population: " + tot_pop); //7_162_119_000
     }
 }
